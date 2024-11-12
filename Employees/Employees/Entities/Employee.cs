c164cf43dcdc6d5da85f93b24bc4cc1d2f76e7d1
Luiz Henrique Dutra <95194764+LuizH-D@users.noreply.github.com>
2024-11-11 22:51:39 -0300

    Add files via upload

---
+++ Employees/Employees/Entities/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text;
+
+
+namespace Employees.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name + " - $" + Payment().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}

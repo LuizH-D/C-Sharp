5982104d5b51c06d956725d6df069a55662aefff
Luiz Henrique Dutra <95194764+LuizH-D@users.noreply.github.com>
2025-01-14 08:01:37 -0300

    Add files via upload

---
+++ CoursesPortal/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace CoursesPortal.Entities {
+    class Student {
+        public int Registration { get; set; }
+        
+
+        public Student() {
+        }
+
+        public Student(int registration) {
+            Registration = registration;
+        }
+
+        public override int GetHashCode() {
+            return Registration.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Student)) {
+                return false;
+            }
+
+            Student other = obj as Student;
+            return Registration.Equals(other.Registration);
+        }
+    }
+}
---
+++ CoursesPortal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CoursesPortal.Entities;
+using System.Collections.Generic;
+
+namespace CoursesPortal {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<Student> set = new HashSet<Student>();
+
+            try {
+                Console.Write("How many students for course Portuguese? ");
+                int course1 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 1; i <= course1; i++) {
+                    Console.Write("Enter Student Registration: ");
+                    int registration = int.Parse(Console.ReadLine());
+                    set.Add(new Student(registration));
+                }
+
+                Console.WriteLine();
+                Console.Write("How many students for course English? ");
+                int course2 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 1; i <= course2; i++) {
+                    Console.Write("Enter Student Registration: ");
+                    int registration = int.Parse(Console.ReadLine());
+                    set.Add(new Student(registration));
+                }
+
+                Console.WriteLine();
+                Console.Write("How many students for course Spanish? ");
+                int course3 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 1; i <= course3; i++) {
+                    Console.Write("Enter Student Registration: ");
+                    int registration = int.Parse(Console.ReadLine());
+                    set.Add(new Student(registration));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"Total students: {set.Count}");
+            }
+            catch (FormatException ex) {
+                Console.WriteLine($"Format error: {ex.Message}");
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+        }
+    }
+}
